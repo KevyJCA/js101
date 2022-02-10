e13d5ac37509a71e4a08fdabde5393df9d6b3f2e
KevyJCA <kevinjohnson82ca@gmail.com>
2022-02-10 13:03:26 -0500

    lesson1

---
+++ script.js
@@ -0,0 +1,26 @@
+//console.log("Hello World!")//
+//console.log("Welcome to My GitHub repository")//
+
+// lesson 1 //
+
+//let scoops = 5;//
+//while (scoops > 0) {//
+  //  document.write("Another scoop!");//
+    //scoops = - 1;//
+//document.write ("Life without icecream isn't the same") 
+//
+
+//99 bottles of beer//
+
+let word="bottles";
+let count=99;
+while (count > 0) {
+console.log (count + "" + word + "of beer on the wall");
+console.log (count + "" + word + "of beer");
+console.log ("take one down pass it around");
+count = count -1;}
+if (count > 0) {
+    console.log (count + "" + word + "of beer on the wall");
+} else {
+    console.log ("no more bottle of beer on the wall");
+}
